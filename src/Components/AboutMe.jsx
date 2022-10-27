d2c69a618cdf21aeef59d5ec593f3c6ae7b3b417
Vivianie-P <vivianieprice@gmail.com>
2022-10-27 13:11:03 -0700

    fixed spacing on each element

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -6,15 +6,13 @@ function AboutMe() {
 		<div className="aboutme-main" id="/">
 			<h1 className="aboutme-title">About Me</h1>
 			<p className="aboutme-ppg">
-				I'm an aspiring frontend software engineer specializing in building websites
-				and applications. My interest in web devlopment started back in 2020 after
-				graduating in the middle of the pandemic. The job market for psychology
-				majors weren't very abundant due to the climate of the world. As a result, I
-				wanted to learn a new skill that would be challenging and engaging. Intially
-				I learned Python from there I learned HTML and CSS. At that point I learned
-				that frontend devlopment was what I enjoyed the most. In my recent projects
-				React was my framework of choice in conjuction with Javascript, CSS and
-				Node.js.
+				My interest in web devlopment started back in 2020 after graduating in the
+				middle of the pandemic. The job market for psychology majors weren't very
+				abundant due to the climate of the world. As a result, I wanted to learn a
+				new skill that would be challenging and engaging. Intially I learned Python
+				from there I learned HTML and CSS. At that point I learned that frontend
+				devlopment was what I enjoyed the most. In my recent projects React was my
+				framework of choice in conjuction with Javascript, CSS and Node.js.
 			</p>
 		</div>
 	);
