cb5e6bfa38455dd910cab555719151db7d95fdea
Vivianie-P <vivianieprice@gmail.com>
2022-11-03 14:27:57 -0700

    Adjusted some sapacing

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -6,7 +6,7 @@ function ContactMe() {
 		<div className="contact-me-container">
 			<h1 className="contact-me-title">Let's network</h1>
 			<div className="inner-container">
-				<h1 className="subtitle">Want to work together?</h1>
+				<h1 className="subtitle">Want to work together or have any questions?</h1>
 				<a className="mailto" id="contact-me" href="mailto:vivianieprice@gmail.com">
 					Contact Me
 				</a>
