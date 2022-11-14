159522138cc96f0ce048b1e395cf8052feedeb08
Vivianie-P <vivianieprice@gmail.com>
2022-11-14 12:13:14 -0800

    Updated skills section completely

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -18,30 +18,28 @@ function AboutMe() {
 				projects React was my framework of choice in conjuction with Javascript, CSS
 				and Node.js.
 			</p>
-			<div className="skills-main" id="/skills">
-				<h1 className="skills-title">Skills</h1>
-				<div className="skills-container" id="skills">
-					<div className="skills__skill">
-						<h2>React</h2>
-					</div>
-					<div className="skills__skill">
-						<h2>JavaScript</h2>
-					</div>
-					<div className="skills__skill">
-						<h2>HTML</h2>
-					</div>
-					<div className="skills__skill">
-						<h2>CSS</h2>
-					</div>
-					<div className="skills__skill">
-						<h2>Python</h2>
-					</div>
-					<div className="skills__skill">
-						<h2>Node.js</h2>
-					</div>
-					<div className="skills__skill">
-						<h2>GIT</h2>
-					</div>
+			<h2 className="skills-title">Here are a few of my skills:</h2>
+			<div className="skills-container">
+				<div className="skills__skill">
+					<h2>React</h2>
+				</div>
+				<div className="skills__skill">
+					<h2>JavaScript</h2>
+				</div>
+				<div className="skills__skill">
+					<h2>HTML</h2>
+				</div>
+				<div className="skills__skill">
+					<h2>CSS</h2>
+				</div>
+				<div className="skills__skill">
+					<h2>Python</h2>
+				</div>
+				<div className="skills__skill">
+					<h2>Node.js</h2>
+				</div>
+				<div className="skills__skill">
+					<h2>GIT</h2>
 				</div>
 			</div>
 		</div>
